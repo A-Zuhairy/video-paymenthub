215710f1a0d5e173dd4727694ae9bdc5a90b7f2d
A-Zuhairy <zuhairy@utem.edu.my>
2026-01-12 11:56:59 +0800

    put music

--- src/MainVideo/MainVideo.jsx
+++ src/MainVideo/MainVideo.jsx
@@ -1,4 +1,11 @@
-import { Sequence, AbsoluteFill, Audio } from "remotion";
+import {
+  Sequence,
+  AbsoluteFill,
+  Audio,
+  staticFile,
+  useCurrentFrame,
+  interpolate,
+} from "remotion";
 import { Scene1 } from "./Scene1";
 import { Scene2 } from "./Scene2";
 import { Scene3 } from "./Scene3";
@@ -12,12 +19,24 @@ import {
   SCENE_4_DURATION,
   SCENE_6_DURATION,
   SCENE_7_DURATION,
-  COLOR_PRIMARY
+  COLOR_PRIMARY,
+  FPS,
+  TOTAL_DURATION,
 } from "./constants";
 
 export const MainVideo = () => {
+  const frame = useCurrentFrame();
   let currentFrame = 0;
-  
+
+  const baseVolume = 0.9;
+  const fadeOutDurationInFrames = FPS;
+  const volume = interpolate(
+    frame,
+    [TOTAL_DURATION - fadeOutDurationInFrames, TOTAL_DURATION],
+    [baseVolume, 0],
+    { extrapolateLeft: "clamp", extrapolateRight: "clamp" }
+  );
+
   const scenes = [
     { component: Scene1, duration: SCENE_1_DURATION },
     { component: Scene2, duration: SCENE_2_DURATION },
@@ -29,6 +48,7 @@ export const MainVideo = () => {
 
   return (
     <AbsoluteFill style={{ backgroundColor: COLOR_PRIMARY }}>
+      <Audio src={staticFile("music/Background.mp3")} volume={volume} />
       {scenes.map((scene, index) => {
         const from = currentFrame;
         currentFrame += scene.duration;
